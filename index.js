d8f83cd699f7fb2d52bd317cfe0d94f4473ba3d7
Andrei Medvedev <medvedevandrei29@yandex.ru>
2021-12-18 15:30:47 +0300

    добавлен readme  и мелкие исправления

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const popupMenu = document.querySelector('.popup-menu');
+const popupMenu = document.querySelector('.popup-menu'); 
 
-function openPopup(popupElement) {             
-    popupElement.classList.add('popup-menu_opened');
+function openPopup(el) {             
+    el.classList.add('popup-menu_opened');
   };
-  function closePopup(popupElement) {            
-    popupElement.classList.remove('popup-menu_opened');
+  function closePopup(el) {            
+    el.classList.remove('popup-menu_opened');
   };
 
 
-const burgMenu = document.querySelector('.header__burg-menu');//находим кнопку редактирования профиля
-const closeMenu = popupMenu.querySelector('.popup-menu__close-button'); //находим кнопку закрытия попапа профиля
-burgMenu.addEventListener('click', function () {                //на кнопку редактирования профиля вешаем функцию открытия попапа            
+const burgMenu = document.querySelector('.header__burg-menu');
+const closeMenu = popupMenu.querySelector('.popup-menu__close-button'); 
+burgMenu.addEventListener('click', function () {                        
   openPopup(popupMenu);
 });
-closeMenu.addEventListener('click', function () {               //на кнопку закрытия профиля вешаем функцию закрытия попапа
+closeMenu.addEventListener('click', function () {               
   closePopup(popupMenu);
 });
